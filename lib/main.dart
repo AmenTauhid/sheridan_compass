8c6ec8d4e5bac523b8f566f1a05bf81d12ebf660
Omar Al-Dulaimi <aldulaom@sheridancollege.ca>
2023-04-05 20:29:34 -0400

    B building floor 1 image implimented. The image is static, will try to allow tile based mapping.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ import 'package:location/location.dart';
 void main() {
   runApp(const IndoorNavigationApp());
 }
+class CustomMarker{
+  late final String label;
+  late final Offset position;
 
+  CustomMarker({ required this.label, required this.position});
+
+}
 class IndoorNavigationApp extends StatelessWidget {
   const IndoorNavigationApp({super.key});
 
@@ -28,6 +34,7 @@ class IndoorNavigationMap extends StatefulWidget {
   _IndoorNavigationMapState createState() => _IndoorNavigationMapState();
 }
 
+/*
 class _IndoorNavigationMapState extends State<IndoorNavigationMap> {
   Location _location = Location();
 
@@ -44,6 +51,46 @@ class _IndoorNavigationMapState extends State<IndoorNavigationMap> {
           subdomains: ['a', 'b', 'c'],
         ),
       ],
+    );
+  }
+}
+*/
+class _IndoorNavigationMapState extends State<IndoorNavigationMap> {
+  // Sample custom markers
+  List<CustomMarker> markers = [
+    CustomMarker(label: 'A', position: const Offset(50, 100)),
+    CustomMarker(label: 'B', position: const Offset(150, 200)),
+    CustomMarker(label: 'C', position: const Offset(250, 300)),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset('assets/B_building_F1.png', fit: BoxFit.cover),
+        ...markers.map((marker) => _buildMarker(marker)),
+      ],
+    );
+  }
+
+  Widget _buildMarker(CustomMarker marker) {
+    return Positioned(
+      left: marker.position.dx,
+      top: marker.position.dy,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+        child: Text(
+          marker.label,
+          style: const TextStyle(
+            fontSize: 14,
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 }
