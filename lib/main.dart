229538cc91d61504d4a1ec6142ff7a1b50abeb72
Omar Al-Dulaimi <aldulaom@sheridancollege.ca>
2023-04-08 18:05:06 -0400

    implimentation of realistic map of outside campus with zoom in and zoom out features, just need to impliment detailed info inside campus when zoomed in into campus.

--- lib/main.dart
+++ lib/main.dart
@@ -3,94 +3,95 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
 
-void main() {
-  runApp(const IndoorNavigationApp());
-}
-class CustomMarker{
-  late final String label;
-  late final Offset position;
-
-  CustomMarker({ required this.label, required this.position});
-
-}
 class IndoorNavigationApp extends StatelessWidget {
-  const IndoorNavigationApp({super.key});
+  const IndoorNavigationApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Indoor Navigation')),
-        body: const IndoorNavigationMap(),
-      ),
+      title: 'Indoor Navigation App',
+      home: const IndoorNavigationMap(),
     );
   }
 }
 
+void main() {
+  runApp(const IndoorNavigationApp());
+}
+
+class CustomMarker {
+  late final String label;
+  late final Offset position;
+
+  CustomMarker({required this.label, required this.position});
+}
+
 class IndoorNavigationMap extends StatefulWidget {
-  const IndoorNavigationMap({super.key});
+  const IndoorNavigationMap({Key? key}) : super(key: key);
 
   @override
   _IndoorNavigationMapState createState() => _IndoorNavigationMapState();
 }
 
-
 class _IndoorNavigationMapState extends State<IndoorNavigationMap> {
   final Location _location = Location();
+
+  // List of LatLng objects to represent the location of each marker
+  final List<LatLng> _markers = [
+    LatLng(43.468523, -79.700456), // Marker 1
+    LatLng(43.468923, -79.700256), // Marker 2
+    LatLng(43.468323, -79.700656), // Marker 3
+  ];
+
+  // Sample custom markers
+  List<CustomMarker> markers = [
+    CustomMarker(label: 'A', position: const Offset(50, 100)),
+    CustomMarker(label: 'B', position: const Offset(150, 200)),
+    CustomMarker(label: 'C', position: const Offset(250, 300)),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
       options: MapOptions(
-        center: LatLng(43.468523, -79.700456), // Replace with your initial coordinates
-        zoom: 18.0, // Set the initial zoom level.
-        maxZoom: 22.0, // Set the maximum zoom level.
+        center: LatLng(43.468523, -79.700456), // Set the initial location of the map
+        zoom: 16.0,
       ),
       layers: [
+        // Add the tile layer to display the map
         TileLayerOptions(
-          urlTemplate: 'https://api.maptiler.com/tiles/46faaaf4-1e47-4c14-8911-3becb667b1b0/{z}/{x}/{y}.png?key=9FoelL2TTrBdRJSCStUK',
+          urlTemplate: 'https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',
+          subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
         ),
+        // Add a marker layer to display the custom markers
+        MarkerLayerOptions(markers: markers.map((marker) {
+          return Marker(
+            point: LatLng(0, 0),
+            builder: (context) => _buildMarker(marker),
+          );
+        }).toList()),
       ],
     );
   }
-}
 
-// class _IndoorNavigationMapState extends State<IndoorNavigationMap> {
-//   // Sample custom markers
-//   List<CustomMarker> markers = [
-//     CustomMarker(label: 'A', position: const Offset(50, 100)),
-//     CustomMarker(label: 'B', position: const Offset(150, 200)),
-//     CustomMarker(label: 'C', position: const Offset(250, 300)),
-//   ];
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Stack(
-//       children: [
-//         Image.asset('assets/B_building_F1.png', fit: BoxFit.cover),
-//         ...markers.map((marker) => _buildMarker(marker)),
-//       ],
-//     );
-//   }
-//
-//   Widget _buildMarker(CustomMarker marker) {
-//     return Positioned(
-//       left: marker.position.dx,
-//       top: marker.position.dy,
-//       child: Container(
-//         decoration: BoxDecoration(
-//           color: Colors.red,
-//           borderRadius: BorderRadius.circular(20),
-//         ),
-//         padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
-//         child: Text(
-//           marker.label,
-//           style: const TextStyle(
-//             fontSize: 14,
-//             color: Colors.white,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  Widget _buildMarker(CustomMarker marker) {
+    return Positioned(
+      left: marker.position.dx,
+      top: marker.position.dy,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+        child: Text(
+          marker.label,
+          style: const TextStyle(
+            fontSize: 14,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
