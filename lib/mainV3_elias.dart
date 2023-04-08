44e5e5c40fb5d94523e8d90650e4a71f06edba04
Elias A <alissand@sheridancollege.ca>
2023-04-08 19:50:49 -0400

    Updated map to be centered upon app start

--- lib/mainV3_elias.dart
+++ lib/mainV3_elias.dart
@@ -17,13 +17,6 @@ void main() {
   runApp(const IndoorNavigationApp());
 }
 
-class CustomMarker {
-  late final String label;
-  late final Offset position;
-
-  CustomMarker({required this.label, required this.position});
-}
-
 class IndoorNavigationMap extends StatefulWidget {
   const IndoorNavigationMap({Key? key}) : super(key: key);
 
@@ -32,55 +25,27 @@ class IndoorNavigationMap extends StatefulWidget {
 }
 
 class _IndoorNavigationMapState extends State<IndoorNavigationMap> {
-  List<CustomMarker> markers = [
-    CustomMarker(label: 'A', position: const Offset(50, 100)),
-    CustomMarker(label: 'B', position: const Offset(150, 200)),
-    CustomMarker(label: 'C', position: const Offset(250, 300)),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PhotoView.customChild(
-        backgroundDecoration: const BoxDecoration(color: Colors.white),
-        minScale: PhotoViewComputedScale.contained,
-        maxScale: 10.0,
-        child: Stack(
-          children: [
-            AspectRatio(
-              aspectRatio: 1.35, // Replace with the correct aspect ratio for your image
-              child: FittedBox(
-                fit: BoxFit.cover,
-                child: Image.asset(
-                  'assets/B_building_F1.png',
-                ),
-              ),
+      body: Container(
+        decoration: const BoxDecoration(color: Colors.white),
+        child: Center(
+          child: SizedBox(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.width / 1.35,
+            child: PhotoView(
+              imageProvider: AssetImage('assets/B_building_F1.png'),
+              backgroundDecoration: const BoxDecoration(color: Colors.white),
+              minScale: PhotoViewComputedScale.contained,
+              maxScale: 10.0,
+              initialScale: PhotoViewComputedScale.contained * 0.8,
             ),
-            ...markers.map((marker) => _buildMarker(marker)),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildMarker(CustomMarker marker) {
-    return Positioned(
-      left: marker.position.dx,
-      top: marker.position.dy,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
-        child: Text(
-          marker.label,
-          style: const TextStyle(
-            fontSize: 14,
-            color: Colors.white,
           ),
         ),
       ),
     );
   }
 }
+
+
