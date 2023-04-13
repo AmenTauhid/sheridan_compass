302d81aa5223cb42f155037aa9f4dea45dff6e13
Ayman Tauhid <tauhida@sheridancollege.ca>
2023-04-12 22:24:18 -0400

    Testing google map API.

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:location/location.dart';
-
-class IndoorNavigationApp extends StatelessWidget {
-const IndoorNavigationApp({Key? key}) : super(key: key);
-
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: 'Indoor Navigation App',
-home: const IndoorNavigationMap(),
-);
-}
-}
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'src/locations.dart' as locations;
 
 void main() {
-runApp(const IndoorNavigationApp());
+  runApp(const MyApp());
 }
 
-class CustomMarker {
-late final String label;
-late final Offset position;
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-CustomMarker({required this.label, required this.position});
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class IndoorNavigationMap extends StatefulWidget {
-const IndoorNavigationMap({Key? key}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
+  }
 
-@override
-_IndoorNavigationMapState createState() => _IndoorNavigationMapState();
-}
-
-class _IndoorNavigationMapState extends State<IndoorNavigationMap> {
-final Location _location = Location();
-
-// List of LatLng objects to represent the location of each marker
-final List<LatLng> _markers = [
-LatLng(43.468523, -79.700456), // Marker 1
-LatLng(43.468923, -79.700256), // Marker 2
-LatLng(43.468323, -79.700656), // Marker 3
-];
-
-// Sample custom markers
-List<CustomMarker> markers = [
-CustomMarker(label: 'A', position: const Offset(50, 100)),
-CustomMarker(label: 'B', position: const Offset(150, 200)),
-CustomMarker(label: 'C', position: const Offset(250, 300)),
-];
-
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-body: InteractiveViewer(
-boundaryMargin: EdgeInsets.all(20),
-minScale: 0.1,
-maxScale: 10,
-child: Stack(
-children: [
-Image.asset(
-'assets/B_building_F1.png',
-fit: BoxFit.cover,
-width: MediaQuery.of(context).size.width,
-height: MediaQuery.of(context).size.height,
-),
-...markers.map((marker) => _buildMarker(marker)),
-],
-),
-),
-);
-}
-
-Widget _buildMarker(CustomMarker marker) {
-return Positioned(
-left: marker.position.dx,
-top: marker.position.dy,
-child: Container(
-decoration: BoxDecoration(
-color: Colors.red,
-borderRadius: BorderRadius.circular(20),
-),
-padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
-child: Text(
-marker.label,
-style: const TextStyle(
-fontSize: 14,
-color: Colors.white,
-),
-),
-),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Google Office Locations'),
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(0, 0),
+            zoom: 2,
+          ),
+          markers: _markers.values.toSet(),
+        ),
+      ),
+    );
+  }
 }
