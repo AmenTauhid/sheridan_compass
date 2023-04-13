1bf4b397fd491bc142ee02848fe6ea5e4f6762fe
Elias A <alissand@sheridancollege.ca>
2023-04-13 15:33:30 -0400

    Improved app usability by implementing zoom in and zoom out and updating drawer

--- lib/mainV3_elias.dart
+++ lib/mainV3_elias.dart
@@ -17,10 +17,27 @@ class _MyAppState extends State<MyApp> {
   final Map<String, Marker> _markers = {};
   int _currentIndex = 0;
 
+  void _zoomIn() {
+    final GoogleMapController? controller = _mapController;
+    if (controller != null) {
+      controller.animateCamera(CameraUpdate.zoomIn());
+    }
+  }
+
+  void _zoomOut() {
+    final GoogleMapController? controller = _mapController;
+    if (controller != null) {
+      controller.animateCamera(CameraUpdate.zoomOut());
+    }
+  }
+
+  GoogleMapController? _mapController;
+
   Future<void> _onMapCreated(GoogleMapController controller) async {
     setState(() {
       _markers.clear();
     });
+    _mapController = controller;
   }
 
   void _onTabTapped(int index) {
@@ -37,62 +54,83 @@ class _MyAppState extends State<MyApp> {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Sheridan Compass 🧭'),
+          title: const Text('Sheridan Compass',
+            style: TextStyle(
+              decoration: TextDecoration.underline,
+            ),
+          ),
+
           centerTitle: true,
           elevation: 2,
         ),
-        body: Column(
+        body: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Locate',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25.0),
-                  ),
-                ),
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(43.46843248599148, -79.70040205206587), // Oakville Campus coordinates
+                zoom: 17, // Adjust zoom level as needed
               ),
+              markers: _markers.values.toSet(),
+              mapType: MapType.satellite,
+              zoomControlsEnabled: false, // Disable default zoom controls
             ),
-            Expanded(
-              child: GoogleMap(
-                onMapCreated: _onMapCreated,
-                initialCameraPosition: const CameraPosition(
-                  target: LatLng(43.46843248599148, -79.70040205206587), // Oakville Campus coordinates
-                  zoom: 17, // Adjust zoom level as needed
-                ),
-                markers: _markers.values.toSet(),
-                mapType: MapType.satellite,
+            Positioned(
+              left: 16, // Adjust position as needed
+              bottom: 16, // Adjust position as needed
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FloatingActionButton(
+                    onPressed: _zoomIn,
+                    child: const Icon(Icons.add),
+                  ),
+                  const SizedBox(height: 6),
+                  FloatingActionButton(
+                    onPressed: _zoomOut,
+                    child: const Icon(Icons.remove),
+                  ),
+                ],
               ),
             ),
           ],
         ),
         drawer: Drawer(
-          child: Column(
-            children: [
-              const SizedBox(height: 50),
-              const Text(
-                'S Compass',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+          child: Container(
+            color: Colors.white,
+            child: Column(
+              children: [
+                const SizedBox(height: 95),
+                const Text(
+                  '🧭',
+                  style: TextStyle(
+                    fontSize: 100,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              const Icon(Icons.explore),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('About'),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Settings'),
-              ),
-              const Spacer(),
-              const Text('2023'),
-              const SizedBox(height: 10),
-            ],
+                const SizedBox(height: 40),
+                const Text(
+                  'Sheridan Compass',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('About'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Settings'),
+                ),
+                const Spacer(),
+                const Text('Ayman, Elias, & Omar'),
+                const Text('April, 2023'),
+                const SizedBox(height: 10),
+              ],
+            ),
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
@@ -104,12 +142,12 @@ class _MyAppState extends State<MyApp> {
               label: 'Explore',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.bookmark),
-              label: 'Saved Locations',
+              icon: Icon(Icons.save),
+              label: 'Saved Trips',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
+              icon: Icon(Icons.history),
+              label: 'History',
             ),
           ],
         ),
